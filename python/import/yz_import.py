ebc84e98498d9507ccee5b1506d17908b845b87f
YaZa <yasin.zamani@gmail.com>
2016-12-18 00:23:46 -0700

    Add 'yz_import'

---
+++ python/import/yz_import.py
@@ -0,0 +1,16 @@
+"""
+YZ_IMPORT try to understand python compiler!
+"""
+def main():
+    """
+    MAIN test module
+    ex.1
+    >>> path = './file01.txt'
+    >>> print(is_valid(path))
+    True
+    """
+    path = './file01.txt'
+    print(is_valid(path))
+
+if __name__ == '__main__':
+    main()
