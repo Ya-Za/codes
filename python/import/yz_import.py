a4715d110e8133c6fdb03400516fdfee8c0aecd6
Yasin <yasin.zamani@gmail.com>
2016-12-19 16:27:21 +0330

    Add class 'Tree'

--- python/import/yz_import.py
+++ python/import/yz_import.py
@@ -6,11 +6,35 @@ def main():
     MAIN test module
     ex.1
     >>> path = './file01.txt'
-    >>> print(is_valid(path))
+    >>> tree = Tree(path)
+    >>> print(tree.is_valid(path))
     True
     """
     path = './file01.txt'
-    print(is_valid(path))
+    tree = Tree(path)
+    print(tree.is_valid())
+
+class Tree(object):
+    """
+    TREE makes a tree from a module with given path.
+    """
+    def __init__(self, path):
+        self.path = path
+        self.root = self.make_tree()
+
+    def make_tree(self):
+        """
+        MAKE_TREE makes a tree from a module with given path.
+        """
+        root = Node('
+
+    def is_valid(self):
+        """
+        IS_VALID check that a module with given 'path' is valid or not.
+        """
+        pass
+
+
 
 if __name__ == '__main__':
     main()
