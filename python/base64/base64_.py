d61f665b2004623d4446bcd7a0714494ffbcb3a9
YaZa <yasin.zamani@gmail.com>
2017-02-14 00:01:31 -0700

    Add 'decode' to 'Base64' class

--- python/base64/base64_.py
+++ python/base64/base64_.py
@@ -7,9 +7,14 @@ def main():
     """
     MAIN
     """
-    str_ = b'yasin'
-    print(Base64.encode(str_))
-    print(codecs.encode(b'yasin', 'base64'))
+    bytearray_ = bytearray(b'yasin')
+
+    b64 = Base64.encode(bytearray_)
+    # b64 = codecs.encode(bytearray_, 'base64')
+    bytearray_ = Base64.decode(b64)
+    print(bytearray_.decode('ascii'))
+    print(b64)
+    print(bytearray_.decode('ascii'))
 
 
 class Base64():
@@ -19,14 +24,15 @@ class Base64():
     codes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 
     @staticmethod
-    def bytearray_to_str(bytearray_):
+    def bytearray_to_str(bytearray_, length):
         """
         BYTEARRAY_TO_STR
         """
-        return ''.join('{0:08b}'.format(byte) for byte in bytearray_)
+        formatstr = '{0:0' + str(length) + 'b}'
+        return ''.join(formatstr.format(byte) for byte in bytearray_)
 
     @staticmethod
-    def split_str(str_, sublength):
+    def split_str(str_, sublength, ignore_residula=False):
         """
         SPLIT_STR
         """
@@ -41,7 +47,7 @@ class Base64():
                 break
             index += sublength
 
-        if index < length:
+        if index < length and not ignore_residula:
             trailing_zero = '0' * (sublength - (length - index))
             res.append(str_[index:] + trailing_zero)
 
@@ -52,10 +58,22 @@ class Base64():
         """
         ENCODE
         """
-        byte_str = Base64.bytearray_to_str(bytearray_)
+        byte_str = Base64.bytearray_to_str(bytearray_, 8)
         splited_str = Base64.split_str(byte_str, 6)
 
         return ''.join(Base64.codes[int(item, 2)] for item in splited_str)
+
+    @staticmethod
+    def decode(str_):
+        """
+        DECODE
+        """
+        bytearray_ = [Base64.codes.index(ch) for ch in str_]
+        byte_str = Base64.bytearray_to_str(bytearray_, 6)
+        splited_str = Base64.split_str(byte_str, 8, True)
+
+        return bytearray(int(item, 2) for item in splited_str)
+
 
 
 if __name__ == '__main__':
