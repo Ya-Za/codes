f816ef503911a0c6804ce02ce72e524064c5b3bb
Yasin <yasin.zamani@gmail.com>
2017-04-16 16:38:52 +0430

    Add `testsuite`

--- matlab/tutorialspoint/computer_graphics/test.m
+++ matlab/tutorialspoint/computer_graphics/test.m
@@ -3,5 +3,8 @@ clear();
 clc();
 
 % Test Suite
-import('matlab.unittest.TestSuite');
-run(TestSuite.fromFolder('tests'));
+% import('matlab.unittest.TestSuite');
+% run(TestSuite.fromFolder('tests'));
+
+suite = testsuite('./tests/TestUtils.m');
+suite.run();
