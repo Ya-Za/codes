3a860c0276e1cc164240a0f7bf162e0bd0d3fb69
Yasin <yasin.zamani@gmail.com>
2017-05-06 18:05:58 +0430

    Add `Tree.equals`

---
+++ javascript/tree/NodeTree.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Node
+ */
+var NodeTree = (function () {
+    function NodeTree(name) {
+        this.name = name;
+        this.parent = null;
+        this.childs = [];
+    }
+    return NodeTree;
+}());
+exports.default = NodeTree;
+//# sourceMappingURL=NodeTree.js.map
