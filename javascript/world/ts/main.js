105629a429dc65a6751bb69f8c008779bd73b66d
YaZa <yasin.zamani@gmail.com>
2017-06-24 00:35:06 +0330

    Remove `world/ts/main.js`

--- javascript/world/ts/main.js
+++
@@ -1,9 +0,0 @@
-// let canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-// let ctx = canvas.getContext("2d");
-// let width = canvas.width;
-// let height = canvas.height;
-// ctx.fillStyle = "red";
-// ctx.fillRect(0, 0, 100, 100);
-// ctx.arc(width / 2, height / 2, 0.1 * height, 0, 2 * Math.PI);
-// ctx.fill();
-console.log("Hello, World!");
