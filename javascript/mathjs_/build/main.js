f29062af9a29117cb0ab280320b777df649d67f0
YaZa <yasin.zamani@gmail.com>
2017-06-24 00:39:14 +0330

    Modify mathjs_ project

--- javascript/mathjs_/build/main.js
+++ javascript/mathjs_/build/main.js
@@ -1,16 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const math = require("mathjs");
-let forces = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-];
-// let sumOfForces = [0, 0];
-// for (let force of forces) {
-//     sumOfForces = math.add(sumOfForces, force) as number[];
-// }
-let sumOfForces = forces.reduce((acc, cur) => math.add(acc, cur), [0, 0]);
-console.log(sumOfForces);
-console.log(math.dotMultiply([1, 2, 3], 2));
+console.log(math.sqrt(-4));
 //# sourceMappingURL=main.js.map
--- javascript/mathjs_/src/main.ts
+++ javascript/mathjs_/src/main.ts
@@ -1,21 +1,3 @@
 import * as math from "mathjs"
 
-let forces = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-];
-
-// let sumOfForces = [0, 0];
-// for (let force of forces) {
-//     sumOfForces = math.add(sumOfForces, force) as number[];
-// }
-
-let sumOfForces = forces.reduce(
-    (acc, cur) => math.add(acc, cur) as number[], 
-    [0, 0]
-);
-
-console.log(sumOfForces);
-
-console.log(math.dotMultiply([1, 2, 3], 2));
+console.log(math.sqrt(-4));
